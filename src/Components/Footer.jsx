df486e1d05a1da34290d30988808a6f78d8473a2
DarkGamer2 <kameer-hosien@outlook.com>
2022-08-23 19:48:18 +0000

    Created a footer

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -2,13 +2,16 @@ import React from 'react'
 import "../Styles/Footer.css"
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faArrowRight } from '@fortawesome/free-solid-svg-icons'
+import { faEnvelope } from '@fortawesome/free-solid-svg-icons'
+import { faWhatsapp } from '@fortawesome/free-brands-svg-icons'
+import { faFacebook } from '@fortawesome/free-brands-svg-icons'
+import { faTwitter } from '@fortawesome/free-brands-svg-icons'
 const Footer = () => {
   return (
     <div>
       <footer>
         <div className="footer-container">
           <div className="col">
-
             <div className="row">
               <h2>LOCATION</h2>
               <span></span>
@@ -34,12 +37,26 @@ const Footer = () => {
            <div className="row">
              <h2>NEWSLETTER</h2>
             <div className="email-input">
+              <FontAwesomeIcon icon={faEnvelope}/>
                <input type="email" name="email" placeholder='johndoe@gmail.com'/>
              <button className="newsletter-button" type="submit"><FontAwesomeIcon icon={faArrowRight}/></button>
             </div>
+            <span></span>
+
+            <div className="icons-container">
+   <div className="icon">
+     <FontAwesomeIcon icon={faWhatsapp} className="whatsapp-icon"/>
+   </div>
+    <div className="icon">
+      <FontAwesomeIcon icon={faFacebook} className="facebook-icon"/>
+    </div>
+   <div className="icon">
+     <FontAwesomeIcon icon={faTwitter} className="twitter-icon"/>
+   </div>
+            </div>
            </div>
           </div>
-        </div>
+          </div>
       </footer>
     </div>
   )
